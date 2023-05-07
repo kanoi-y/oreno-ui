a464206322c9e77a2a37b9243eaac6bf2983170e
kanoi-y <furafuratonbo@gmail.com>
2023-05-08 00:25:15 +0900

    ✨ Switch.vueを実装

---
+++ src/components/switch/Switch.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+type Props = {
+  modelValue: boolean;
+  disabled?: boolean;
+};
+const props = defineProps<Props>();
+
+type Emits = {
+  (e: 'update:modelValue', value: boolean): void;
+};
+const emits = defineEmits<Emits>();
+</script>
+<template>
+  <button
+    type="button"
+    role="switch"
+    :disabled="disabled"
+    :aria-pressed="props.modelValue"
+    :data-orenoui-state="props.modelValue ? 'on' : 'off'"
+    @click="() => emits('update:modelValue', !props.modelValue)"
+  >
+    <slot />
+  </button>
+</template>
