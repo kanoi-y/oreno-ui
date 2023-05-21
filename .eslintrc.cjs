59e087c4b423f62e9021f7982f52270bf6ad2c88
kanoi-y <furafuratonbo@gmail.com>
2023-05-22 01:10:54 +0900

    ➕ storybookをセットアップ

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,9 @@
 require('@rushstack/eslint-patch/modern-module-resolution');
-
 module.exports = {
   root: true,
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/eslint-config-airbnb-with-typescript',
-    'prettier',
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/eslint-config-airbnb-with-typescript', 'plugin:storybook/recommended', 'prettier'],
   ignorePatterns: ['**/*.js'],
   rules: {
-    'vue/multi-word-component-names': off,
-  },
+    'vue/multi-word-component-names': off
+  }
 };
