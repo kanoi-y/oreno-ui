cda9b8b5291d6074bd5646ee91117b20c2db864f
kanoi-y <furafuratonbo@gmail.com>
2023-05-27 19:40:13 +0900

    ✨ disabled時のswitchのストーリーを作成

--- src/components/switch/Switch.story.vue
+++ src/components/switch/Switch.story.vue
@@ -9,9 +9,23 @@ const enabled = ref(false);
     <template #controls>
       <HstCheckbox v-model="enabled" title="enabled" />
     </template>
-    <Variant>
+    <Variant title="default">
       <Switch
         v-model="enabled"
+        :class="enabled ? 'bg-orange-700' : 'bg-orange-500'"
+        class="relative inline-flex h-9 w-[72px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+      >
+        <span
+          aria-hidden="true"
+          :class="enabled ? 'translate-x-9' : 'translate-x-0'"
+          class="pointer-events-none inline-block h-8 w-8 transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out"
+        />
+      </Switch>
+    </Variant>
+    <Variant title="disabled">
+      <Switch
+        v-model="enabled"
+        :disabled="true"
         :class="enabled ? 'bg-orange-700' : 'bg-orange-500'"
         class="relative inline-flex h-9 w-[72px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
       >
