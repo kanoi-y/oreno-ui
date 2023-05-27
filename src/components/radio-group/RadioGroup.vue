a638f5d08bf1042120fcbfddf449e750848f55a7
kanoi-y <furafuratonbo@gmail.com>
2023-05-27 22:17:05 +0900

    ✨ RadioCroup.vueを作成

---
+++ src/components/radio-group/RadioGroup.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { provide, ref } from 'vue';
+
+
+// FIXME: vue3.3にupgradeした際に型をジェネリクスに修正する。
+type Props = {
+  modelValue: string;
+}
+
+type Emits = {
+  (e: 'update:modelValue', value: string): void;
+};
+
+const props = defineProps<Props>();
+
+const emits = defineEmits<Emits>();
+
+const selectedValue = ref(props.modelValue);
+
+const updateSelectedValue = (value: string) => {
+  selectedValue.value = value;
+  emits('update:modelValue', value);
+};
+
+provide('selectedValue', { selectedValue, updateSelectedValue });
+
+</script>
+
+<template>
+  <div role="radiogroup">
+    <slot></slot>
+  </div>
+</template>
