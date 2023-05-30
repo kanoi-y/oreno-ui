a7ed80947418f2afd81f453cc3b47fe4c7532fb0
kanoi-y <furafuratonbo@gmail.com>
2023-05-30 22:11:06 +0900

    ✨ RadioGroupのストーリーを作成

---
+++ src/components/radio-group/RadioGroup.story.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import RadioGroup from './RadioGroup.vue';
+import RadioGroupItem from './RadioGroupItem.vue';
+
+const cats = [
+  {
+    id: '1',
+    name: 'Tama',
+  },
+  {
+    id: '2',
+    name: 'Mike',
+  },
+  {
+    id: '3',
+    name: 'Kuro',
+  },
+];
+
+const selectedCat = ref('1');
+</script>
+<template>
+  <Story title="RadioGroup">
+    <Variant title="default">
+      <RadioGroup v-model="selectedCat" class="flex flex-col gap-2">
+        <RadioGroupItem
+          v-for="cat in cats"
+          :key="cat.id"
+          :value="cat.id"
+          class="flex gap-2 items-center"
+        >
+          <div
+            class="w-6 h-6 rounded-full border-2 border-orange-400 flex justify-center items-center"
+          >
+            <div
+              v-if="selectedCat === cat.id"
+              class="w-2 h-2 rounded-full bg-orange-500"
+            ></div>
+          </div>
+          <span class="text-md text-slate-300">{{ cat.name }}</span>
+        </RadioGroupItem>
+      </RadioGroup>
+    </Variant>
+  </Story>
+</template>
