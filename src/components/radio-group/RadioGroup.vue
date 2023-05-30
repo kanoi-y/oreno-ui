358f443095cd192f352a000bde884df67556b595
kanoi-y <furafuratonbo@gmail.com>
2023-05-31 00:12:40 +0900

    ✨ 矢印キーの押下で、値が切り替わるように実装

--- src/components/radio-group/RadioGroup.vue
+++ src/components/radio-group/RadioGroup.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import { provide, ref } from 'vue';
-
+import { computed, provide, ref } from 'vue';
 
 // FIXME: vue3.3にupgradeした際に型をジェネリクスに修正する。
 type Props = {
   modelValue: string;
-}
+};
 
 type Emits = {
   (e: 'update:modelValue', value: string): void;
@@ -16,18 +15,53 @@ const props = defineProps<Props>();
 const emits = defineEmits<Emits>();
 
 const selectedValue = ref(props.modelValue);
+const items = ref<string[]>([]);
+const currentIndex = computed(() => items.value.indexOf(selectedValue.value));
+
+const registerItem = (value: string) => {
+  items.value.push(value);
+};
 
 const updateSelectedValue = (value: string) => {
   selectedValue.value = value;
   emits('update:modelValue', value);
 };
 
-provide('selectedValue', { selectedValue, updateSelectedValue });
+const handleKeydown = (event: KeyboardEvent) => {
+  if (currentIndex.value === -1) return;
+  if (
+    event.key === 'Up' ||
+    event.key === 'ArrowUp' ||
+    event.key === 'Left' ||
+    event.key === 'ArrowLeft'
+  ) {
+    if (currentIndex.value > 0) {
+      updateSelectedValue(items.value[currentIndex.value - 1]);
+      return;
+    }
+    updateSelectedValue(items.value[items.value.length - 1]);
+  }
 
+  if (
+    event.key === 'Down' ||
+    event.key === 'ArrowDown' ||
+    event.key === 'Right' ||
+    event.key === 'ArrowRight'
+  ) {
+    if (currentIndex.value < items.value.length - 1) {
+      updateSelectedValue(items.value[currentIndex.value + 1]);
+      return;
+    }
+    updateSelectedValue(items.value[0]);
+  }
+};
+
+provide('selectedValue', { selectedValue, updateSelectedValue });
+provide('registerItem', registerItem);
 </script>
 
 <template>
-  <div role="radiogroup">
+  <div role="radiogroup" @keydown.prevent="handleKeydown">
     <slot></slot>
   </div>
 </template>
--- src/components/radio-group/RadioGroupItem.vue
+++ src/components/radio-group/RadioGroupItem.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { Ref, computed, inject } from 'vue';
+import { Ref, computed, inject, onMounted, ref } from 'vue';
+//TODO: 矢印キーの押下でフォーカスも移動するように実装する
 
 type Props = {
   value: string;
@@ -11,11 +12,20 @@ const { selectedValue, updateSelectedValue } = inject('selectedValue') as {
   selectedValue: Ref<string>;
   updateSelectedValue: (value: string) => void;
 };
+const registerItem = inject('registerItem') as (value: string) => void;
+
+const radioRef = ref(null);
+
 const isChecked = computed(() => selectedValue.value === props.value);
+
+onMounted(() => {
+  registerItem(props.value);
+});
 </script>
 <template>
   <div
     role="radio"
+    ref="radioRef"
     :aria-checked="isChecked"
     :tabindex="isChecked ? 0 : -1"
     :data-orenoui-state="isChecked ? 'checked' : 'unchecked'"
