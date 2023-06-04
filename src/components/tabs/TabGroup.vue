b9183db457be6affbfce2f62efe3fc63c8ce81f1
kanoi-y <furafuratonbo@gmail.com>
2023-06-04 23:41:47 +0900

    ✨ TabGroup.vueの雛形を実装

---
+++ src/components/tabs/TabGroup.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { provide, ref } from 'vue';
+
+type Props = {
+  defaultValue?: string;
+}
+
+const props = defineProps<Props>();
+
+//TODO: defaultValueが存在しない場合、最初の要素が選択されるようにする。
+const selectedTab = ref(props.defaultValue);
+const updateSelectedTab = (value: string) => {
+  selectedTab.value = value;
+};
+
+provide('selectedTab', { selectedTab, updateSelectedTab });
+
+</script>
+<template>
+  <div>
+   <slot />
+  </div>
+</template>
